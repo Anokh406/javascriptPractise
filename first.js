d7c6991e240125a98a05fc135a546c08f005ede0
Anokh406 <anokhkashyap@gmail.com>
2024-10-24 00:03:40 +0530

    javascript practise code

---
+++ first.js
@@ -0,0 +1,32 @@
+//  let sum=0;
+//  for(let i=1; i<=5; i++){
+//   sum=sum+i;
+// }
+// console.log(sum);
+// let str="anokhkashyap@gmail.com";
+// for(let i of str){
+//     console.log("i=",i);
+// }
+// for(let i=1; i<=100; i++){
+//     if(i %2 == 0){
+//         console.log(i)
+//     }else{
+//     }
+// }
+// let gameNum=25;
+// let userNum= prompt("Guess the game number :");
+// while(userNum != gameNum){
+//   userNum=  prompt("Wrong Guess :)");
+// }
+// console.log("Congratulations you enter the correct number");
+// let button1=document.getElementById('btn-1');
+// var button2=document.querySelector('.btn-2');
+// button1.style.color="red";
+// button2.style.color="green"; 
+// document.title="this is our javascript web page";
+
+let heart=document.querySelector('.heart');
+// heart.style.color="r";
+let btn1=document.getElementById('btn-1');
+btn1.style.borderRadius="5px";
+btn1.style.color="green";
